c4cf557721bd45ffdedf329cccbcc5ff41983471
Spencer Williams <spencerwi@gmail.com>
2023-12-03 17:17:08 -0500

    Day 2 solution

---
+++ day2/Lib.fs
@@ -0,0 +1,101 @@
+module Lib
+open System
+open System.Text.RegularExpressions
+
+/// A RegEx active pattern for easier regexing
+let (|RegEx|_|) p i =
+    let m = Regex.Match(i, p) in
+    if m.Success then
+        Some m.Groups
+    else
+        None
+
+module ElfGame = begin
+    type Supplies = {
+        reds: int;
+        greens: int
+        blues: int
+    } with
+        static member parse_round (round_str : string) : Supplies =
+            let mutable reds = 0 in
+            let mutable greens = 0 in
+            let mutable blues = 0 in
+            round_str.Split(", ")
+            |> Array.iter (fun group -> 
+                match group with 
+                | RegEx "(?<count>\d+) red" g -> 
+                    reds <- (g["count"].Value |> int)
+                | RegEx "(?<count>\d+) blue" g ->
+                    blues <- (g["count"].Value |> int)
+                | RegEx "(?<count>\d+) green" g ->
+                    greens <- (g["count"].Value |> int)
+                | _ -> ()
+            )
+            { reds = reds; greens = greens; blues = blues }
+
+        member this.power () : int =
+            this.reds * this.greens * this.blues
+
+
+    type Game = {
+        id: int;
+        rounds : Supplies array
+    }
+        with 
+            static member parse (line : string) : Game =
+                let [|game_id_part; rounds_str|] = line.Split ":" in
+                let game_id = 
+                    match game_id_part with
+                    | RegEx "Game (?<game_id>\d+)" g ->
+                        g["game_id"].Value |> int
+                    | _ -> failwith ("Invalid game ID part: " + game_id_part)
+                in
+                { id = game_id; rounds = (rounds_str.Split ";") |> Array.map Supplies.parse_round }
+
+            member this.can_be_played_with (supplies : Supplies) =
+                this.rounds 
+                |> Seq.forall (fun round ->
+                    round.reds <= supplies.reds &&
+                    round.greens <= supplies.greens &&
+                    round.blues <= supplies.blues
+                )
+
+            member this.minimum_supplies_required () = 
+                let mutable min_reds = 0 in
+                let mutable min_greens = 0 in
+                let mutable min_blues = 0 in
+                for round in this.rounds do
+                    if round.reds > min_reds then min_reds <- round.reds
+                    if round.greens > min_greens then min_greens <- round.greens
+                    if round.blues > min_blues then min_blues <- round.blues
+                done;
+                { reds = min_reds; greens = min_greens; blues = min_blues }
+
+end
+
+module Puzzle = begin
+    let part1 (input: string seq) =
+        let bag : ElfGame.Supplies = {
+            reds = 12; greens = 13; blues = 14
+        } in
+        let games = 
+            input 
+            |> Seq.filter (not << String.IsNullOrWhiteSpace)
+            |> Seq.map ElfGame.Game.parse
+        in 
+        games
+            |> Seq.filter (fun game -> game.can_be_played_with bag)
+            |> Seq.map (fun game -> game.id)
+            |> Seq.sum
+
+    let part2 (input: string seq) =
+        let games = 
+            input 
+            |> Seq.filter (not << String.IsNullOrWhiteSpace)
+            |> Seq.map ElfGame.Game.parse
+        in 
+        games
+            |> Seq.map (fun game -> game.minimum_supplies_required())
+            |> Seq.map (fun game_supplies -> game_supplies.power())
+            |> Seq.sum
+end
---
+++ day2/Tests.fs
@@ -0,0 +1,84 @@
+module Tests
+open Lib
+open type ElfGame.Supplies
+open type ElfGame.Game
+
+open NUnit.Framework
+open FsUnit
+open System
+
+let sample_input_raw = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+
+let sample_input = sample_input_raw.Split "\n" |> Seq.toArray
+
+[<TestFixture>]
+type ``Supplies tests`` ()=
+    [<Test>]
+    member this.``It should determine power correctly`` ()=
+        let supplies = { reds = 4; greens = 2; blues = 6 } in
+        supplies.power() 
+        |> should equal 48
+
+
+[<TestFixture>]
+type ``ElfGame tests`` ()=
+    [<Test>]
+    member this.``It should parse a single game correctly`` ()=
+        ElfGame.Game.parse sample_input.[1]
+        |> should equal ({
+            id = 1
+            rounds = [|
+                { blues = 3; reds = 4; greens = 0 };
+                { reds = 1; greens = 2; blues = 6 };
+                { greens = 2; reds = 0; blues = 0 }
+            |]
+        })
+
+    [<Test>]
+    member this.``It should evaluate playability correctly`` ()=
+        let example_bag = {
+            reds = 12
+            greens = 13
+            blues = 14
+        } in
+        let games = 
+            sample_input
+            |> Array.filter (not << String.IsNullOrWhiteSpace)
+            |> Array.map ElfGame.Game.parse
+        in
+        games |> Array.map (fun game -> game.can_be_played_with example_bag)
+        |> should equal [| true; true; false; false; true |]
+
+    [<Test>]
+    member this.``It should determine minimum supplies required correctly`` ()=
+        let games = 
+            sample_input
+            |> Array.filter (not << String.IsNullOrWhiteSpace)
+            |> Array.map ElfGame.Game.parse
+        in
+        games |> Array.map (fun game -> game.minimum_supplies_required())
+        |> should equal [|
+            { reds = 4; greens = 2; blues = 6 };
+            { reds = 1; greens = 3; blues = 4 };
+            { reds = 20; greens = 13; blues = 6 };
+            { reds = 14; greens = 3; blues = 15 };
+            { reds = 6; greens = 3; blues = 2}
+        |]
+
+[<TestFixture>]
+type ``Tests for solution`` ()=
+    [<Test>]
+    member this.``It should solve part 1`` ()=
+        Puzzle.part1 sample_input
+        |> should equal 8
+
+    [<Test>]
+    member this.``It should solve part 2`` ()=
+        Puzzle.part2 sample_input
+        |> should equal 2286
